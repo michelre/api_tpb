1ce2de6166ff74f65aa83c4ddb9f0c5328306c04
michelre <remi.michel38@gmail.com>
2014-08-11 12:13:19 +0200

    first commit with one route

---
+++ api.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+import tpb_utils as utils
+app = Flask(__name__)
+
+@app.route("/search")
+def search():
+	query = request.args.get('query')
+	offset = request.args.get('offset') if request.args.get('offset') else 0
+	return jsonify(utils.get_torrents_by_query(query, offset))
+
+
+if __name__ == "__main__":
+	app.run()
+
---
+++ tpb_utils.py
@@ -0,0 +1,18 @@
+from tpb import TPB
+from tpb import CATEGORIES, ORDERS
+
+t = TPB('https://thepiratebay.org') # create a TPB object with default domain
+
+def get_torrents_by_query(query, offset):
+	torrents = []
+	result = {}
+	
+	for torrent in t.search(query).order(ORDERS.SEEDERS.DES).page(offset):
+		torrent_info = {}
+		torrent_info["title"] = torrent.title
+		torrent_info["size"] = torrent.size
+		torrent_info["download_link"] = torrent.magnet_link
+		torrent_info["seeders"] = torrent.seeders
+		torrents.append(torrent_info)
+	result["result"] = torrents
+	return result
