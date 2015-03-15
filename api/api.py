7f1d5aa2851c5355cdadab35432ecb0bcd9589ae
remimichel <remi.michel38@gmail.com>
2015-03-15 14:24:39 +0100

    refacto api to take torrent-hunter

--- api/api.py
+++ api/api.py
@@ -20,7 +20,8 @@ def home():
 
 @app.route("/auth/<tracker>", methods=['POST', 'OPTIONS'])
 def auth(tracker):
-	if tracker == "t411":
+	print "POST"
+	if tracker == "t411":		
 		username = request.form['username']
 		password = request.form['password']
 		result = utils.t411_auth(username, password)
@@ -31,52 +32,49 @@ def auth(tracker):
 def search(tracker):
 	offset = request.args.get('offset') if request.args.get('offset') else 0
 	query = request.args.get('q')
-	if tracker == "t411":
-		authorization = request.args.get('authorization')
-		result = utils.get_torrents_t411_by_query(query, offset, authorization)		
-	else:
-		result = utils.get_torrents_tpb_by_query(query, offset)		
+	authorization = request.args.get('authorization')
+	result = utils.get_torrents_by_query(tracker, query, offset, authorization)
 	return Response(json.dumps(result), mimetype='application/json')
 
-@app.route("/torrents/<tracker>/info")
-def info(tracker):
-	id = request.args.get('id')
-	torrent_info = {}
+@app.route("/torrents/<tracker>/info/<id>")
+def info(tracker, id):
+	torrent_info = ''
 	if tracker == "t411":
 		authorization = request.args.get('authorization')
 		torrent_info = utils.get_torrent_info_t411(id, authorization)
 	else:
-		torrent_info = utils.get_torrent_info_by_url(id)
-	return Response(json.dumps(torrent_info), mimetype='application/json')
+		torrent_info = utils.get_torrent_info_torrent_hunter(id)
+	return Response(json.dumps({'details' : torrent_info}), mimetype='application/json')
 
 @app.route("/categories/<tracker>")
 def categories(tracker):
 	if tracker == "t411":
 		categories = {"path": "CATEGORIES", "name":"CATEGORIES", "has_children": True, "categories": 
-		[{"path": "CATEGORIES.ALL", "has_children": False, "name": "ALL", "categories":[]}, {"path": "CATEGORIES.TOPS", "has_children": False, "name": "TOPS", "categories":[{"path": "CATEGORIES.TOP100", "has_children": False, "name": "TOP100", "categories":[]}]}]}
+		[{"path": "CATEGORIES.ALL", "has_children": False, "name": "ALL", "categories":[]}, {"path": "CATEGhostIES.TOPS", "has_children": False, "name": "TOPS", "categories":[{"path": "CATEGORIES.TOP100", "has_children": False, "name": "TOP100", "categories":[]}]}]}
 	else:
 		children_of = request.args.get('children_of') if request.args.get('children_of') else 'CATEGORIES'
 		depth = int(request.args.get('depth')) if request.args.get('depth') else 100
 		categories = utils.get_categories(children_of, depth)	
 	return Response(json.dumps(categories), mimetype='application/json')
 
-@app.route("/torrents/<tracker>/top/<category>")
-def torrentsPerCategory(tracker, category):
-	if tracker == "t411":
-		authorization = request.args.get('authorization')
-		tops = utils.get_top_100_t411(authorization)
-	else:
-		tops = utils.get_torrents_per_category(category)
+@app.route("/torrents/<tracker>/tops")
+def torrentsPerCategory(tracker):
+	authorization = request.args.get('authorization')
+	tops = utils.get_tops(tracker, authorization)
 	return Response(json.dumps(tops), mimetype='application/json')
 
 @app.route("/torrents/<tracker>/download/<id>")
 def download(tracker, id):
 	if tracker == "t411":
 		authorization = request.args.get('authorization')
-		magnet_link = utils.get_torrent_file(id, authorization)
+		magnet_link = utils.get_torrent_file_t411(id, authorization)
+		return Response(json.dumps(magnet_link), mimetype='application/json')
+	if tracker == 'torrent-hunter':
+		id_splitted = id.split("_")
+		magnet_link = utils.get_torrent_file_torrent_hunter(id_splitted[0], id_splitted[1])
 		return Response(json.dumps(magnet_link), mimetype='application/json')
 	return Response(None, mimetype='application/json')
 
 
 if __name__ == '__main__':
-        app.run(debug=True)
+        app.run(debug=True, host='0.0.0.0')
--- api/tpb_utils.py
+++ api/tpb_utils.py
@@ -1,15 +1,17 @@
-from tpb import TPB
-from tpb import CATEGORIES, ORDERS
+#from tpb import TPB
+#from tpb import CATEGORIES, ORDERS
 import re
 import inspect
 import urllib
 import requests
-from pyquery import PyQuery as pq
-from lxml import etree
+#from pyquery import PyQuery as pq
+#from lxml import etree
 import json
 import utils
 
-t = TPB('https://thepiratebay.org')
+#t = TPB('https://thepiratebay.org')
+
+base_url = {'t411' : 'http://api.t411.me', 'torrent-hunter' : 'http://localhost:9090/app_dev.php/api/v1'}
 
 def sizeof_fmt(num):
     for x in ['bytes','KB','MB','GB','TB']:
@@ -22,11 +24,12 @@ def t411_auth(username, password):
 	return result_req.text
 
 def make_dict_from_torrent(torrent):
-	torrent_info = {}
-	torrent_info["title"] = torrent.title
-	torrent_info["size"] = torrent.size
-	torrent_info["download_link"] = torrent.magnet_link
-	torrent_info["url"] = "%s" % torrent.url
+	torrent_info = {}	
+	torrent_info['title'] = torrent['title']
+	torrent_info["size"] = torrent['size']
+	torrent_info["id"] = torrent["tracker"] + '_' + torrent['slug']
+	torrent_info["seeders"] = torrent['seeds']
+	torrent_info["leechers"] = torrent['leechs']
 	return torrent_info
 
 def make_dict_from_torrent_t411(torrent):	
@@ -34,63 +37,34 @@ def make_dict_from_torrent_t411(torrent):
 	torrent_info["title"] = torrent["name"]
 	torrent_info["size"] = sizeof_fmt(float(torrent["size"]))
 	torrent_info["id"] = torrent["id"]
+	torrent_info["seeders"] = torrent['seeders']
+	torrent_info["leechers"] = torrent['leechers']
 	return torrent_info
 
-def get_torrents_tpb_by_query(query, offset):
-	torrents = []	
-	for torrent in t.search(query).order(ORDERS.SEEDERS.DES).page(offset):
-		torrent_info = make_dict_from_torrent(torrent)
-		#torrent_info["description"] = torrent.info
-		torrents.append(torrent_info)
-	global_torrents = torrents		
-	return torrents
-
-def get_torrents_t411_by_query(query, offset, authorization):
-	torrents = []	
-	result_req = requests.get('http://api.t411.me/torrents/search/' + query + '?offset=' + str(offset) + '&limit=30', headers={'Authorization': authorization})	
-	for torrent in json.loads(result_req.text)["torrents"]:
-		torrent_info = make_dict_from_torrent_t411(torrent)
-		torrents.append(torrent_info)
-	return torrents
-
-
-def find_all_categories(path, acc, depth):		
-	if depth == 0:
-		return acc
-	if inspect.isclass(eval(path)):
-		for name in dir(eval(path)):
-			if not name.startswith('_'):
-				acc[-1]["categories"].append({"name": name, "categories":[], "path": path+"."+name, "has_children": inspect.isclass(eval(path+'.'+name))})
-				find_all_categories(path+'.'+name, acc[-1]["categories"], depth-1)
-	return acc
-
-def get_category(categories, category, index):
-	array_category = str(category).split(".")	
-	for c in categories:
-		array_c = c["path"].split(".")
-		if array_c == array_category[:index]:
-			if len(array_category) == index:
-				return c
-			else:
-				return get_category(c["categories"], category, index+1)
-
-def get_categories(children_of, depth):
-	if depth < 0:
-		depth = 0
-	depth_children_of = len(children_of.split(".")) - 1
-	all_categories = find_all_categories('CATEGORIES', [{"name": "CATEGORIES", "categories": [], "path": "CATEGORIES", "has_children": True}], depth + depth_children_of)[0]
-	if(children_of == "CATEGORIES"):		
-		return all_categories
-	else:
-		return get_category(all_categories["categories"], children_of, 2)
-
-def get_top_100_t411(authorization):
+def get_torrents_by_query(tracker, query, offset, authorization):
 	torrents = []
-	result_req = requests.get('http://api.t411.me/torrents/top/100',  headers={'Authorization': authorization})
+	if tracker == 't411':
+		search_url = base_url['t411'] + '/torrents/search/' + query + '&offset=' + str(offset) + '&limit=30'
+	else:
+		search_url = base_url['torrent-hunter'] + '/search?query=' + query + '&offset=' + str(offset) + '&limit=30'
+
+	result_req = requests.get(search_url, headers={'Authorization': authorization})
 	print result_req.text
-	for torrent in json.loads(result_req.text):
-		print torrent
-		torrent_info = make_dict_from_torrent_t411(torrent)
+	for torrent in json.loads(result_req.text)["torrents"]:
+		torrent_info = make_dict_from_torrent_t411(torrent) if tracker == 't411' else make_dict_from_torrent(torrent)
+		torrents.append(torrent_info)
+	return torrents
+
+def get_tops(tracker, authorization):
+	torrents = []
+	if tracker == 't411':
+		search_url = base_url['t411'] + '/torrents/top/100'
+	else:
+		search_url = base_url['torrent-hunter'] + '/torrents/top'
+	result_req = requests.get(search_url, headers={'Authorization': authorization})
+	print search_url
+	for torrent in json.loads(result_req.text):		
+		torrent_info = make_dict_from_torrent_t411(torrent) if tracker == 't411' else make_dict_from_torrent(torrent)		
 		torrents.append(torrent_info)
 	return torrents
 
@@ -106,21 +80,20 @@ def get_torrents_per_category(category):
 def get_torrent_info_t411(torrent_id, authorization):
 	torrent_info = {}
 	result_req = requests.get('http://api.t411.me/torrents/details/' + torrent_id,  headers={'Authorization': authorization})
-	torrent_info["description"] = json.loads(result_req.text)["description"]
+	torrent_info['description'] = json.loads(result_req.text)["description"]
 	return torrent_info
 
-def get_torrent_info_by_url(torrent_url):	
+def get_torrent_info_torrent_hunter(torrent_id):		
 	torrent_info = {}
-	r = requests.get(torrent_url)
-	d = pq(r.text)
-	torrent_info["description"] = d("pre").html()
-	return torrent_info
+	id = torrent_id.split("_")
+	r = requests.get(base_url['torrent-hunter'] + "/torrent/" + id[0] + "/details/" + id[1])
+	return json.loads(r.text)["details"]
 
-def get_torrent_file(id, authorization):
+def get_torrent_file_t411(id, authorization):
 	r = requests.get('http://api.t411.me/torrents/download/' + id,  headers={'Authorization': authorization})
 	return {'magnet_link': utils.createMagnetLinkFromTorrent(r.content)}
 
-
-
-
+def get_torrent_file_torrent_hunter(tracker_provider, slug):
+	r = requests.get(base_url['torrent-hunter'] + '/torrent/' + tracker_provider + '/' + slug)
+	return {'magnet_link': json.loads(r.text)['downloadLink']}
 
